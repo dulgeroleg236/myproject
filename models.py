f22b265beee443f48f428a11bc1ae2977e66da0e
dulgeroleg236 <64584759+dulgeroleg236@users.noreply.github.com>
2025-04-25 19:16:40 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse, NoReverseMatch
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+    url = models.CharField(max_length=200, blank=True)
+    named_url = models.CharField(max_length=200, blank=True)
+
+    def get_absolute_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.named_url)
+            except NoReverseMatch:
+                return '#'
+        return self.url or '#'
+
+    def __str__(self):
+        return self.title
